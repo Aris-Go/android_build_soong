3a6ffad5281a730ab4758813e927a4482a8e8127
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-13 11:03:48 -0700

    soong: Add ARIS_VERSION to dumpvars
    Change-Id: I24b29442f041aec451a173db404f542060ac67d2

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -145,6 +145,7 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 
 // Variables to print out in the top banner
 var BannerVars = []string{
+	"ARIS_VERSION",
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
 	"PRODUCT_INCLUDE_TAGS",
